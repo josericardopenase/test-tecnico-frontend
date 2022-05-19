acf8f50f9a9d9c319fd1dbcd77f44093d25e78fe
Juan Pablo Royo Sales <juanpablo.royo@gmail.com>
2022-05-20 00:55:33 +0200

    feat: Finishing API

--- src/api/Spotify/index.js
+++ src/api/Spotify/index.js
@@ -1,8 +1,31 @@
 import api from '../../config.js'
 import axios from 'axios';
 
-
 const client = axios.create({ baseURL: api.api.baseUrl, json: true });
+
+const tokenExpired = token => Object.keys(token).length === 0 || token.expired + token.timeInSec < 30
+
+const getToken = () => {
+  const token = localStorage.getItem('token')
+  if(token){
+    try{
+      return JSON.parse(token)
+    }catch(err){
+      return {}
+    }
+  }else{
+    return {}
+  }
+}
+
+client.interceptors.request.use((config) => {
+  const token = getToken()
+  if (tokenExpired(token)) {
+    auth();
+  }
+  return config;
+});
+
 
 const auth = () => {
   const formData = new URLSearchParams()
@@ -18,10 +41,38 @@ const auth = () => {
         json: true
   }
   axios.post(api.api.authUrl, formData, options).then(function(response) {
-          console.log(response);
+          let token = Object.assign({}, response.data);
+          token.timeInSec = Math.floor(Date.now() / 1000);
+          localStorage.setItem('token', JSON.stringify(token))
       }).catch(function(err) {
-        console.log(err)
+         throw new Error(`HTTP Error on auth token ${err}`)
       })
 }
 
-export default auth
+export default {
+    async execute(method, resource, respObj, data) {
+        const token = getToken()
+        return client({
+          method,
+          url: resource,
+          data,
+          crossdomain: true ,
+          headers: { "Authorization": `Bearer ${token.access_token}` }
+        }).then(req => {
+          return req.data[respObj].items
+        })
+      },
+
+      getNewReleases() {
+        return this.execute('get', 'browse/new-releases', 'albums')
+      },
+
+      getFeaturedPlaylists() {
+        return this.execute('get', 'browse/featured-playlists', 'playlists')
+      },
+
+      getCategories() {
+        return this.execute('get', 'browse/categories', 'categories')
+      },
+
+}
--- src/routes/Discover/components/Discover.js
+++ src/routes/Discover/components/Discover.js
@@ -1,31 +1,29 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import DiscoverBlock from './DiscoverBlock/components/DiscoverBlock';
 import '../styles/_discover.scss';
-import auth from '../../../api';
+import Spotify from '../../../api';
 
-export default class Discover extends Component {
-  constructor() {
-    super();
+export default function Discover(props) {
 
-    this.state = {
-      newReleases: [],
-      playlists: [],
-      categories: []
+  const [newReleases, setNewReleases] = useState([])
+  const [playlists, setPlaylists] = useState([])
+  const [categories, setCategories] = useState([])
+
+  useEffect(() => {
+    Promise.all([
+      Spotify.getNewReleases().then(setNewReleases),
+      Spotify.getFeaturedPlaylists().then(setPlaylists),
+      Spotify.getCategories().then(setCategories),
+    ]);
+    return () => {
     };
+  }, []);
 
-    auth();
-  };
-
-
-  render() {
-    const { newReleases, playlists, categories } = this.state;
-
-    return (
+ return (
       <div className="discover">
         <DiscoverBlock text="RELEASED THIS WEEK" id="released" data={newReleases} />
         <DiscoverBlock text="FEATURED PLAYLISTS" id="featured" data={playlists} />
         <DiscoverBlock text="BROWSE" id="browse" data={categories} imagesKey="icons" />
       </div>
     );
-  }
 }
